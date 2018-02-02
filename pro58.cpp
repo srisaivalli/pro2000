42dff380102db1362301fafbde28503097d8f606
valli <500060228@DDN.UPES.AC.IN>
2018-02-02 11:13:24 +0530

    first commit

---
+++ pro58.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char str[40];
+  int a,b,sum;
+  cout<<"enter your name";
+  cin>>str;
+  cout<<"enter the values of two subjects";
+  cin>>a>>b;
+  sum=a+b;
+  cout<<"your name is"<<str;
+  cout<<endl<<"and your sum is"<<sum;
+}
