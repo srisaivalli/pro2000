0b3ca35399e6fcff450e43e7a331756b4c196fe5
valli <500060228@DDN.UPES.AC.IN>
2018-02-06 11:46:15 +0530

    commiting for the first time

--- pro58.cpp
+++ pro58.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
   char str[40];
-  int a,b,sum;
+  int a,b,avg;
   cout<<"enter your name";
   cin>>str;
   cout<<"enter the values of two subjects";
   cin>>a>>b;
-  sum=a+b;
+  avg=(a+b)/2;
   cout<<"your name is"<<str;
   cout<<endl<<"and your sum is"<<sum;
 }
---
+++ pro9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std()
+	int main()
+{
+	char[50]str;
+	int a,b,avg;
+	cout<<"enter your name /n";
+	cin>>str;
+	cout<<"enter the values of a,b /n";
+	cin>>a>>b;
+	avg=(a+b)/2;
+	cout<<"your name is"<<str;
+	cout<<endl<<"your avg is"<<avg;
+}
+
